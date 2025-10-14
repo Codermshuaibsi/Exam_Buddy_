cb17b0042571ef4656b767e5ee1d067064f2aca1
Mohd Shuaib <heyshuaib43@gmail.com>
2025-10-14 10:39:01 +0530

    add smtp server using brevo

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -58,7 +58,8 @@ exports.register = async (req, res) => {
         });
         // Mail transporter (use a transactional email provider in production)
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: "smtp-relay.brevo.com",
+            port: 587,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
